b125486d3290589d11d2c467906d09bc60efa424
Martin Mikeštík <145228112+martin-18246@users.noreply.github.com>
2024-09-05 08:19:44 +0200

    Create InputManager.cs

---
+++ InputManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float orbitingDistance;
+    public float orbitingHeight;
+    public float timePerRevolution;
+
+    private float startTime;
+    private float angleRadians;
+
+    public enum CameraMode
+    {
+        MouseMovement,
+        Orbiting,
+    }
+
+    public CameraMode cameraMode;
+
+    void Start()
+    {
+        startTime = Time.time;
+        cameraMode = CameraMode.MouseMovement;
+    }
+
+    void Update()
+    {
+        ChangeCameraMode();
+
+        if (cameraMode == CameraMode.Orbiting) Orbit();
+    }
+
+    private void ChangeCameraMode()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (cameraMode != CameraMode.Orbiting) cameraMode = CameraMode.Orbiting; else cameraMode = CameraMode.MouseMovement;
+            transform.position = new Vector3(orbitingDistance, orbitingHeight, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.M)) cameraMode = CameraMode.MouseMovement;
+    }
+
+
+    private float CalculateAngleRadiansBasedOnTime()
+    {
+        return ( ( (Time.time - startTime) % timePerRevolution ) / timePerRevolution) * 2 * Mathf.PI;
+    }
+
+    private void Orbit()
+    {
+        angleRadians = CalculateAngleRadiansBasedOnTime();
+        float xPosition = Mathf.Sin(angleRadians) * orbitingDistance;
+        float zPosition = Mathf.Cos(angleRadians) * orbitingDistance;
+
+        transform.position = new Vector3(xPosition, orbitingHeight, zPosition);
+        transform.LookAt(Vector3.zero);
+    }
+
+}
